4e6fae108e69d38488dd03556a0384e2084d93b9
Chip Jarred <chipjarred@gmail.com>
2021-03-18 21:50:14 -0500

    Added grep to install.bash to filter out .DS_Store files.

--- Templates/install.bash
+++ Templates/install.bash
@@ -2,4 +2,4 @@
 
 TEMPLATE_FOLDER="${HOME}/Library/Developer/Xcode/Templates"
 
-find . -type f | grep -v install\.bash | cpio -pmd "${TEMPLATE_FOLDER}"
+find . -type f | grep -v install\.bash | grep -v '\.DS_Store' | cpio -pmd "${TEMPLATE_FOLDER}"
