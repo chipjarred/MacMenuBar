2899ccee9826f6da09db53ec57d44a09808000aa
Chip Jarred <chipjarred@gmail.com>
2022-06-08 18:27:33 -0500

    Changed minimum macOS version from 10.15 to 10.12, allowing MacMenuBar to be used with non-SwiftUI applications/frameworks that need more backward macOS compatibility than a SwiftUI app would.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MacMenuBar",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
